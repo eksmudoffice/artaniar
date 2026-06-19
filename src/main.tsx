319404366e4e2dd33d19e3fe7c0bdbffa78b0cdf
[dyad] <eksmudoffice@gmail.com>
2026-06-19 11:54:53 +0800

    [dyad] Tambah app-shell loader untuk hilangkan blank saat initial load - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+const shell = document.getElementById("app-shell");
+if (shell) shell.remove();
+
 createRoot(document.getElementById("root")!).render(<App />);
